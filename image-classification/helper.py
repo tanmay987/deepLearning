6905ccb8d5131e43fb252d717cdc1c4525e0041d
tanmaysinha987 <tanmay.sinha@springernature.com>
2017-08-01 01:04:32 +0530

    "Modified after completing the assignments"

--- image-classification/helper.py
+++ image-classification/helper.py
@@ -1,165 +1,250 @@
-import pickle
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelBinarizer
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+
+    <title>helper.py (editing)</title>
+    <link rel="shortcut icon" type="image/x-icon" href="/static/base/images/favicon.ico?v=97c6417ed01bdc0ae3ef32ae4894fd03">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="/static/components/jquery-ui/themes/smoothness/jquery-ui.min.css?v=9b2c8d3489227115310662a343fce11c" type="text/css" />
+    <link rel="stylesheet" href="/static/components/jquery-typeahead/dist/jquery.typeahead.min.css?v=7afb461de36accb1aa133a1710f5bc56" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    
+<link rel="stylesheet" href="/static/components/codemirror/lib/codemirror.css?v=f25e9a9159e54b423b5a8dc4b1ab5c6e">
+<link rel="stylesheet" href="/static/components/codemirror/addon/dialog/dialog.css?v=c89dce10b44d2882a024e7befc2b63f5">
+
+    <link rel="stylesheet" href="/static/style/style.min.css?v=974839a888beb55bbba87883fafd90fa" type="text/css"/>
+    
+
+    <link rel="stylesheet" href="/custom/custom.css" type="text/css" />
+    <script src="/static/components/es6-promise/promise.min.js?v=f004a16cb856e0ff11781d01ec5ca8fe" type="text/javascript" charset="utf-8"></script>
+    <script src="/static/components/requirejs/require.js?v=6da8be361b9ee26c5e721e76c6d4afce" type="text/javascript" charset="utf-8"></script>
+    <script>
+      require.config({
+          
+          urlArgs: "v=20170731190252",
+          
+          baseUrl: '/static/',
+          paths: {
+            'auth/js/main': 'auth/js/main.min',
+            custom : '/custom',
+            nbextensions : '/nbextensions',
+            kernelspecs : '/kernelspecs',
+            underscore : 'components/underscore/underscore-min',
+            backbone : 'components/backbone/backbone-min',
+            jquery: 'components/jquery/jquery.min',
+            bootstrap: 'components/bootstrap/js/bootstrap.min',
+            bootstraptour: 'components/bootstrap-tour/build/js/bootstrap-tour.min',
+            'jquery-ui': 'components/jquery-ui/ui/minified/jquery-ui.min',
+            moment: 'components/moment/moment',
+            codemirror: 'components/codemirror',
+            termjs: 'components/xterm.js/dist/xterm',
+            typeahead: 'components/jquery-typeahead/dist/jquery.typeahead.min',
+          },
+	  map: { // for backward compatibility
+	    "*": {
+		"jqueryui": "jquery-ui",
+	    }
+	  },
+          shim: {
+            typeahead: {
+              deps: ["jquery"],
+              exports: "typeahead"
+            },
+            underscore: {
+              exports: '_'
+            },
+            backbone: {
+              deps: ["underscore", "jquery"],
+              exports: "Backbone"
+            },
+            bootstrap: {
+              deps: ["jquery"],
+              exports: "bootstrap"
+            },
+            bootstraptour: {
+              deps: ["bootstrap"],
+              exports: "Tour"
+            },
+            "jquery-ui": {
+              deps: ["jquery"],
+              exports: "$"
+            }
+          },
+          waitSeconds: 30,
+      });
+
+      require.config({
+          map: {
+              '*':{
+                'contents': 'services/contents',
+              }
+          }
+      });
+
+      define("bootstrap", function () {
+          return window.$;
+      });
+
+      define("jquery", function () {
+          return window.$;
+      });
+
+      define("jqueryui", function () {
+          return window.$;
+      });
+
+      define("jquery-ui", function () {
+          return window.$;
+      });
+      // error-catching custom.js shim.
+      define("custom", function (require, exports, module) {
+          try {
+              var custom = require('custom/custom');
+              console.debug('loaded custom.js');
+              return custom;
+          } catch (e) {
+              console.error("error loading custom.js", e);
+              return {};
+          }
+      })
+    </script>
+
+    
+    
+
+</head>
+
+<body class="edit_app "
+ 
+data-base-url="/"
+data-file-path="image-classification/helper.py"
+
+  
+ 
+
+>
+
+<noscript>
+    <div id='noscript'>
+      Jupyter Notebook requires JavaScript.<br>
+      Please enable it to proceed.
+  </div>
+</noscript>
+
+<div id="header">
+  <div id="header-container" class="container">
+  <div id="ipython_notebook" class="nav navbar-brand pull-left"><a href="/tree" title='dashboard'><img src='/static/base/images/logo.png?v=641991992878ee24c6f3826e81054a0f' alt='Jupyter Notebook'/></a></div>
+
+  
+  
+  
+
+    <span id="login_widget">
+      
+    </span>
+
+  
+
+  
+
+  
+
+<span id="save_widget" class="pull-left save_widget">
+    <span class="filename"></span>
+    <span class="last_modified"></span>
+</span>
 
 
-def _load_label_names():
-    """
-    Load the label names from file
-    """
-    return ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+  </div>
+  <div class="header-bar"></div>
+
+  
+
+<div id="menubar-container" class="container">
+  <div id="menubar">
+    <div id="menus" class="navbar navbar-default" role="navigation">
+      <div class="container-fluid">
+          <p  class="navbar-text indicator_area">
+          <span id="current-mode" >current mode</span>
+          </p>
+        <button type="button" class="btn btn-default navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <i class="fa fa-bars"></i>
+          <span class="navbar-text">Menu</span>
+        </button>
+        <ul class="nav navbar-nav navbar-right">
+          <li id="notification_area"></li>
+        </ul>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">File</a>
+              <ul id="file-menu" class="dropdown-menu">
+                <li id="new-file"><a href="#">New</a></li>
+                <li id="save-file"><a href="#">Save</a></li>
+                <li id="rename-file"><a href="#">Rename</a></li>
+                <li id="download-file"><a href="#">Download</a></li>
+              </ul>
+            </li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Edit</a>
+              <ul id="edit-menu" class="dropdown-menu">
+                <li id="menu-find"><a href="#">Find</a></li>
+                <li id="menu-replace"><a href="#">Find &amp; Replace</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Key Map</li>
+                <li id="menu-keymap-default"><a href="#">Default<i class="fa"></i></a></li>
+                <li id="menu-keymap-sublime"><a href="#">Sublime Text<i class="fa"></i></a></li>
+                <li id="menu-keymap-vim"><a href="#">Vim<i class="fa"></i></a></li>
+                <li id="menu-keymap-emacs"><a href="#">emacs<i class="fa"></i></a></li>
+              </ul>
+            </li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">View</a>
+              <ul id="view-menu" class="dropdown-menu">
+              <li id="toggle_header" title="Show/Hide the logo and notebook title (above menu bar)">
+              <a href="#">Toggle Header</a></li>
+              <li id="menu-line-numbers"><a href="#">Toggle Line Numbers</a></li>
+              </ul>
+            </li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Language</a>
+              <ul id="mode-menu" class="dropdown-menu">
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="lower-header-bar"></div>
 
 
-def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
-    """
-    Load a batch of the dataset
-    """
-    with open(cifar10_dataset_folder_path + '/data_batch_' + str(batch_id), mode='rb') as file:
-        batch = pickle.load(file, encoding='latin1')
+</div>
 
-    features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
-    labels = batch['labels']
-
-    return features, labels
+<div id="site">
 
 
-def display_stats(cifar10_dataset_folder_path, batch_id, sample_id):
-    """
-    Display Stats of the the dataset
-    """
-    batch_ids = list(range(1, 6))
-
-    if batch_id not in batch_ids:
-        print('Batch Id out of Range. Possible Batch Ids: {}'.format(batch_ids))
-        return None
-
-    features, labels = load_cfar10_batch(cifar10_dataset_folder_path, batch_id)
-
-    if not (0 <= sample_id < len(features)):
-        print('{} samples in batch {}.  {} is out of range.'.format(len(features), batch_id, sample_id))
-        return None
-
-    print('\nStats of batch {}:'.format(batch_id))
-    print('Samples: {}'.format(len(features)))
-    print('Label Counts: {}'.format(dict(zip(*np.unique(labels, return_counts=True)))))
-    print('First 20 Labels: {}'.format(labels[:20]))
-
-    sample_image = features[sample_id]
-    sample_label = labels[sample_id]
-    label_names = _load_label_names()
-
-    print('\nExample of Image {}:'.format(sample_id))
-    print('Image - Min Value: {} Max Value: {}'.format(sample_image.min(), sample_image.max()))
-    print('Image - Shape: {}'.format(sample_image.shape))
-    print('Label - Label Id: {} Name: {}'.format(sample_label, label_names[sample_label]))
-    plt.axis('off')
-    plt.imshow(sample_image)
+<div id="texteditor-backdrop">
+<div id="texteditor-container" class="container"></div>
+</div>
 
 
-def _preprocess_and_save(normalize, one_hot_encode, features, labels, filename):
-    """
-    Preprocess data and save it to file
-    """
-    features = normalize(features)
-    labels = one_hot_encode(labels)
-
-    pickle.dump((features, labels), open(filename, 'wb'))
+</div>
 
 
-def preprocess_and_save_data(cifar10_dataset_folder_path, normalize, one_hot_encode):
-    """
-    Preprocess Training and Validation Data
-    """
-    n_batches = 5
-    valid_features = []
-    valid_labels = []
-
-    for batch_i in range(1, n_batches + 1):
-        features, labels = load_cfar10_batch(cifar10_dataset_folder_path, batch_i)
-        validation_count = int(len(features) * 0.1)
-
-        # Prprocess and save a batch of training data
-        _preprocess_and_save(
-            normalize,
-            one_hot_encode,
-            features[:-validation_count],
-            labels[:-validation_count],
-            'preprocess_batch_' + str(batch_i) + '.p')
-
-        # Use a portion of training batch for validation
-        valid_features.extend(features[-validation_count:])
-        valid_labels.extend(labels[-validation_count:])
-
-    # Preprocess and Save all validation data
-    _preprocess_and_save(
-        normalize,
-        one_hot_encode,
-        np.array(valid_features),
-        np.array(valid_labels),
-        'preprocess_validation.p')
-
-    with open(cifar10_dataset_folder_path + '/test_batch', mode='rb') as file:
-        batch = pickle.load(file, encoding='latin1')
-
-    # load the test data
-    test_features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
-    test_labels = batch['labels']
-
-    # Preprocess and Save all test data
-    _preprocess_and_save(
-        normalize,
-        one_hot_encode,
-        np.array(test_features),
-        np.array(test_labels),
-        'preprocess_test.p')
 
 
-def batch_features_labels(features, labels, batch_size):
-    """
-    Split features and labels into batches
-    """
-    for start in range(0, len(features), batch_size):
-        end = min(start + batch_size, len(features))
-        yield features[start:end], labels[start:end]
 
 
-def load_preprocess_training_batch(batch_id, batch_size):
-    """
-    Load the Preprocessed Training data and return them in batches of <batch_size> or less
-    """
-    filename = 'preprocess_batch_' + str(batch_id) + '.p'
-    features, labels = pickle.load(open(filename, mode='rb'))
-
-    # Return the training data in batches of size <batch_size> or less
-    return batch_features_labels(features, labels, batch_size)
+    
 
 
-def display_image_predictions(features, labels, predictions):
-    n_classes = 10
-    label_names = _load_label_names()
-    label_binarizer = LabelBinarizer()
-    label_binarizer.fit(range(n_classes))
-    label_ids = label_binarizer.inverse_transform(np.array(labels))
 
-    fig, axies = plt.subplots(nrows=4, ncols=2)
-    fig.tight_layout()
-    fig.suptitle('Softmax Predictions', fontsize=20, y=1.1)
+    <script src="/static/edit/js/main.min.js?v=3f26c490a03e64a774b6f266300dac6e" type="text/javascript" charset="utf-8"></script>
 
-    n_predictions = 3
-    margin = 0.05
-    ind = np.arange(n_predictions)
-    width = (1. - 2. * margin) / n_predictions
 
-    for image_i, (feature, label_id, pred_indicies, pred_values) in enumerate(zip(features, label_ids, predictions.indices, predictions.values)):
-        pred_names = [label_names[pred_i] for pred_i in pred_indicies]
-        correct_name = label_names[label_id]
 
-        axies[image_i][0].imshow(feature)
-        axies[image_i][0].set_title(correct_name)
-        axies[image_i][0].set_axis_off()
+</body>
 
-        axies[image_i][1].barh(ind + margin, pred_values[::-1], width)
-        axies[image_i][1].set_yticks(ind + margin)
-        axies[image_i][1].set_yticklabels(pred_names[::-1])
-        axies[image_i][1].set_xticks([0, 0.5, 1.0])
+</html>
